af796a3d01aced4ec9d44f04351500d5b556129c
sherrenjie <114548511+sherrenjie@users.noreply.github.com>
2024-02-12 07:58:36 +0000

    finished centering img and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
 import { useState } from "react";
 import "./App.css";
 
-const phrases = {
-  "No :(": "Are you sure?",
-  "Really? :(": "I will be really sad",
-  "Don't do this to me": "How can you do this to me?",
-  "I am going to cry": "You are going to break my hear",
-  "Please no:(": "Think again, huh?",
-  "You don't want my love? <3": "You sure?",
-  "Please :(": "Are you sure?",
-  "I will not do this": "Are you sure?",
-};
-
 function App() {
   const [noCount, setNoCount] = useState(0);
   const [yesPressed, setYesPressed] = useState(false);
-  const yesButtonSize = noCount * 20 + 16;
+  const yesButtonSize = noCount * 20 + 15;
 
   function handleYesClick() {
     setYesPressed(true);
@@ -23,6 +12,25 @@ function App() {
   function handleNoClick() {
     setNoCount(noCount + 1);
   }
+
+  const phrases = [
+    "No :(",
+    "Are you sure?",
+    "Really? :(",
+    "I will be really sad",
+    "Don't do this to me",
+    "How can you do this to me?",
+    "I am going to cry",
+    "You are going to break my hear",
+    "Please no:(",
+    "Think again, huh?",
+    "You don't want my love? <3",
+    "You sure?",
+    "Please :(",
+    "Are you sure?",
+    "I will not do this",
+    "Are you sure?",
+  ];
 
   function getNoButtonText() {
     return phrases[Math.min(noCount, phrases.length - 1)];
@@ -32,8 +40,8 @@ function App() {
     <div className="valentine-container">
       {yesPressed ? (
         <>
-          <img src="src/assets/200w.gif" />
-          <div className="text">Yeay!</div>
+          <img className="yeay" src="src/assets/200w.gif" />
+          <div className="t">YEAY!</div>
         </>
       ) : (
         <>
@@ -41,8 +49,8 @@ function App() {
             className="kiss"
             src="src/assets/3ce884bb29ad1909a2c253354497420f.gif"
           />
-          <div className="text">Will you be my valentine 🌹?</div>
-          <div className="both-Button">
+          <div className="t">Will you be my valentine?</div>
+          <div className="Button">
             <button
               className="yesButton"
               style={{
@@ -53,7 +61,13 @@ function App() {
             >
               YES
             </button>
-            <button className="NoButton" style={{}} onClick={handleNoClick}>
+            <button
+              className="NoButton"
+              style={{
+                backgroundColor: "#FF3700",
+              }}
+              onClick={handleNoClick}
+            >
               {getNoButtonText()}
             </button>
           </div>
